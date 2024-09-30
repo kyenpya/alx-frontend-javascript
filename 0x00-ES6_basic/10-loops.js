ccec0314a03d1f5853b3a85c0387dcaa9a189b3a
Kyenpya Gutap <138694074+kyenpya@users.noreply.github.com>
2024-09-30 12:30:33 +0100

    Delete 0x00-ES6_basic directory

--- 0x00-ES6_basic/10-loops.js
+++
@@ -1,7 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-  for (let [idx, value] of array.entries()) {
-    array[idx] = appendString + value;
-  }
-
-  return array;
-}
