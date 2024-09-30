4e9d85bcc0f3e96e14ac8b51f72c773546899eab
kyenpya <kyenpyagutap@gmail.com>
2024-09-30 18:30:56 +0100

    Task 1 again

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true; // Using let to declare task2
 
   if (trueOrFalse) {
-    task = true; // Assigning new value to task without re-declaring
-    task2 = false; // Assigning new value to task2 without re-declaring
+    task = false; // Assigning new value to task without re-declaring
+    task2 = true; // Assigning new value to task2 without re-declaring
   }
 
   return [task, task2];
