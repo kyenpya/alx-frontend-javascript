003671b347e87b6ccee1cb739cd2c79644a7a7ad
kyenpya <kyenpyagutap@gmail.com>
2024-10-02 20:08:03 +0100

    task 6 again

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,17 @@
+// 6-final-user.js
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) => {
-    return results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    }));
-  });
+  const promises = [
+    signUpUser(firstName, lastName).then((value) => ({
+      status: 'fulfilled',
+      value,
+    })),
+    uploadPhoto(fileName)
+      .then((value) => ({ status: 'fulfilled', value }))
+      .catch((error) => ({ status: 'rejected', value: error.message })),
+  ];
+
+  return Promise.all(promises);
 }
