0631698028aff1da6991a99d7ef0964907e2b6d0
kyenpya <kyenpyagutap@gmail.com>
2024-10-03 13:43:15 +0100

    fixing task 9 again

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -13,14 +13,11 @@ export class HolbertonClass {
   }
 }
 
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = holbertonClass; // Correctly assign the passed argument
+    this._holbertonClass = holbertonClass;
   }
 
   get fullName() {
@@ -28,7 +25,7 @@ export class StudentHolberton {
   }
 
   get holbertonClass() {
-    return this._holbertonClass; // Return the instance variable
+    return this._holbertonClass;
   }
 
   get fullStudentDescription() {
@@ -36,7 +33,9 @@ export class StudentHolberton {
   }
 }
 
-// Create student instances after class definitions
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
