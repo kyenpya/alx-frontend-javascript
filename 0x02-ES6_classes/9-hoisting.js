60ee1140fda7e4ed5b3b5f7f748dc47d4a3eafbf
kyenpya <kyenpyagutap@gmail.com>
2024-10-03 13:40:18 +0100

    fixes on task 9

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -4,50 +4,43 @@ export class HolbertonClass {
     this._location = location;
   }
 
-  // Getter for year
   get year() {
     return this._year;
   }
 
-  // Getter for location
   get location() {
     return this._location;
   }
 }
 
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = holbertonClass;  // Correctly assign holbertonClass
+    this._holbertonClass = holbertonClass; // Correctly assign the passed argument
   }
 
-  // Getter for fullName
   get fullName() {
     return `${this._firstName} ${this._lastName}`;
   }
 
-  // Getter for holbertonClass
   get holbertonClass() {
-    return this._holbertonClass;
+    return this._holbertonClass; // Return the instance variable
   }
 
-  // Getter for fullStudentDescription
   get fullStudentDescription() {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
-// Create class instances
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
-// Create student instances
+// Create student instances after class definitions
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-// Export the list of students
 export const listOfStudents = [student1, student2, student3, student4, student5];
